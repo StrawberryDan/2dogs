98749f4f7c2aa37f595e5813a28d8a3b22cbebd9
Dan Morris <dm986@bath.ac.uk>
2021-07-09 13:16:04 +0100

    Graphics test now uses Catch2

--- graphics/Test.cpp
+++ graphics/Test.cpp
@@ -1,64 +1,45 @@
-#include <iostream>
+#include <catch2/catch_all.hpp>
 
 #include <GL/gl3w.h>
 #include <loguru.h>
-
-#define TEST(function) \
-    if (function()) {  \
-        success++;               \
-        std::cout << "TEST: [" << #function << "] SUCCESS" << std::endl; \
-    } else { \
-        std::cerr << "TEST: [" << #function << "] FAILED" << std::endl;\
-    } \
-    total++;
-
-#define TEST_ASSERT(x) if (!(x)) return false
-#define TEST_COMPLETE return true
-
 #include <graphics/Window.hpp>
 
-namespace Window {
-    bool CreationTest() {
-        Graphics::Window win("Test Window");
-        win.MakeCurrent();
-        while (!win.ShouldClose()) { 
-            win.PollEvents();
-            glClear(GL_COLOR_BUFFER_BIT);
-            win.SwapBuffers();
-        }
-        TEST_COMPLETE;
-    }
-
-    bool MultipleWindows() {
-        Graphics::Window win1("Test Window 1");
-        Graphics::Window win2("Test Window 2");
-        Graphics::Window win3("Test Window 3");
-
-        while (!(win1.ShouldClose() && win2.ShouldClose() && win3.ShouldClose())) {
-            win1.MakeCurrent();
-            glClearColor(1.0, 0.0, 0.0, 1.0);
-            glClear(GL_COLOR_BUFFER_BIT);
-            win2.MakeCurrent();
-            glClearColor(0.0, 1.0, 0.0, 1.0);
-            glClear(GL_COLOR_BUFFER_BIT);
-            win3.MakeCurrent();
-            glClearColor(0.0, 0.0, 1.0, 1.0);
-            glClear(GL_COLOR_BUFFER_BIT);
-            win1.PollEvents();
-            win2.PollEvents();
-            win3.PollEvents();
-            win1.SwapBuffers();
-            win2.SwapBuffers();
-            win3.SwapBuffers();
-        }
-        TEST_COMPLETE;
+TEST_CASE("Create Window") {
+    Graphics::Window win("Test Window");
+    win.MakeCurrent();
+    while (!win.ShouldClose()) { 
+        win.PollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        win.SwapBuffers();
     }
 }
 
-int main() {
-    unsigned int success = 0, total = 0;
-    TEST(Window::CreationTest)
-    TEST(Window::MultipleWindows)
+TEST_CASE("Multiple Windows") {
+    Graphics::Window win1("Test Window 1");
+    Graphics::Window win2("Test Window 2");
+    Graphics::Window win3("Test Window 3");
 
-    return (success == total) ? 0 : -1;
+    while (!(win1.ShouldClose() && win2.ShouldClose() && win3.ShouldClose())) {
+        win1.MakeCurrent();
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        win2.MakeCurrent();
+        glClearColor(0.0, 1.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        win3.MakeCurrent();
+        glClearColor(0.0, 0.0, 1.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        win1.PollEvents();
+        win2.PollEvents();
+        win3.PollEvents();
+        win1.SwapBuffers();
+        win2.SwapBuffers();
+        win3.SwapBuffers();
+    }
+}
+
+
+int main(int argc, const char **argv) {
+    auto session = Catch::Session();
+    session.run(argc, argv);
 }
