0de93193f4caf22a552877f0fb28fa00ea223f16
Dan Morris <dm986@bath.ac.uk>
2021-08-18 18:20:36 +0100

    Commented Texture.hpp

--- graphics/Texture.hpp
+++ graphics/Texture.hpp
@@ -6,17 +6,25 @@ namespace Graphics {
 	/// Class representing a 2D Texture in OpenGL.
 	class Texture {
 	private:
+		/// Handle to the opengl texture
 		unsigned int handle;
 
 	public:
+		/// Makes a texture from an image.
 		Texture(const Image &image);
+		/// Disable copy constructor.
 		Texture(const Texture &) = delete;
+		/// Define move constructor.
 		Texture(Texture && other);
+		/// Cleanup destructor.
 		~Texture();
 
+		/// Disable copy assignment.
 		Texture &operator=(const Texture &other) = delete;
+		/// Define move assignment
 		Texture &operator=(Texture &&other);
-
+		
+		/// Binds the texture to the GL texture slot.
 		void bind();
 	};
 }
